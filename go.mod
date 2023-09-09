6f828ae5ed63a86986deb9b4051e6d31fc272c51
mdjOoy <mdjoynalabedin2003@gmail.com>
2023-09-09 13:44:56 +0600

    ccc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/golearning
+
+go 1.21.1
--- type.go
+++ type.go
@@ -15,4 +15,21 @@ func main() {
 
 	fmt.Println(coderLevel)
 	fmt.Printf("%T\n", coderLevel)
+
+	type person struct {
+		name string
+		age  int
+	}
+	you := person{"joy", 20}
+	fmt.Println(you)
+
+	fmt.Printf("%T\n", you)
+	x := 5
+	ptr := &x
+	fmt.Printf("The type of ptr is %T and the value is %v\n", ptr, ptr)
+
+	x = 3
+	x += 2
+	x++
+	fmt.Println(x)
 }
