95bacfc2de11d773a8a1019fbaaead99ae02413e
mdjOoy <mdjoynalabedin2003@gmail.com>
2023-09-14 22:32:36 +0600

    file

--- files/main.go
+++ files/main.go
@@ -2,36 +2,23 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	file, err := os.OpenFile("b.txt", os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println(text)
+		if text == "exit" {
+			fmt.Println("exiting scaning...")
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
-	bufferedwritter := bufio.NewWriter(file)
-	bs := []byte{97, 98, 99}
-	byteswritten, err := bufferedwritter.Write(bs)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Bytes written in buffer(not file) :", byteswritten)
-
-	bytesAbaiable := bufferedwritter.Available()
-
-	log.Println("Bytes avaibale in buffer:", bytesAbaiable)
-	byteswritten, err = bufferedwritter.WriteString("\njust a random string")
-	if err != nil {
-		log.Fatal(err)
-	}
-	unflushedBufferSize := bufferedwritter.Buffered()
-	log.Printf("Bytes wriiten in buffered: %d\n", unflushedBufferSize)
-
-	bufferedwritter.Flush()
-	bufferedwritter.Reset(bufferedwritter)
-	bytesAbaiable = bufferedwritter.Available()
-	log.Println(bytesAbaiable)
 }
