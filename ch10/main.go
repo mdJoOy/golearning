0dbad890ce17d162c38327decf89ee1a85f981fa
mdjOoy <mdjoynalabedin2003@gmail.com>
2023-09-19 15:41:50 +0600

    complete challenges on interfaces

--- ch10/main.go
+++ ch10/main.go
@@ -2,22 +2,17 @@ package main
 
 import "fmt"
 
-type book struct {
-    title string
-    price float64
+type cube struct {
+	edge float64
 }
 
-// method for book type
-func (b *book) changePrice(p float64) {
-    b.price = p
+func (c cube) volume() float64 {
+	return c.edge * c.edge * c.edge
 }
 
 func main() {
-    // book value
-    bestBook := book{title: "The Trial by Kafka", price: 9.9}
-
-    // changing the price
-    bestBook.changePrice(11.99)
-
-    fmt.Printf("Book's price:%.2f\n", bestBook.price) // no change
+	var x interface{}
+	x = cube{edge: 5}
+	v := x.(cube).volume()
+	fmt.Printf("Cube Volume: %v\n", v)
 }
